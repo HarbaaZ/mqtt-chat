468f3111660b5a7638e615ec88661fc8365477d8
Mathis Kazek <mathis.kazek@lamie-mutuelle.fr>
2023-04-25 17:05:41 +0200

    Connect brocker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import mqtt from 'precompiled-mqtt'
 
 function App() {
+  const [client, setClient] = useState();
+
+  const connectToBrocker = async () => {
+    const client = await mqtt.connect(
+      'wss://7420d6065a7040db80383dfd7dd31e5c.s2.eu.hivemq.cloud:8884/mqtt',
+      {
+        username: 'mathis',
+        password: 'aL2ykWyvPG!Ttw.'
+      },
+    );
+  
+    client.on('connect', () => {
+      setClient(client)
+      console.log('Connected');
+    });
+  }
+
+  connectToBrocker()
+
   return (
     <div className="App">
-       
+      Test
     </div>
   );
 }
---
+++ src/components/Register.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Register = ({ client }) => {
+    const [messages, setMessages] = useState([])
+    const [message, setMessage] = useState('')
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Register
